6a5f5e4b662af784190af97c2f8ce3cdfa9354a6
sanghamitra-git26 <sanghamitra.sm21@gmail.com>
2025-07-09 14:30:07 +0530

    IT IS FUNCTION AND ARRAY

---
+++ externaljs.js
@@ -0,0 +1,10 @@
+var t = 20;
+var s = 20;
+// console(t * s);
+document.write(t * s);
+
+// to know the datatype
+var h=["HTML" ,"CSS" ,"JS"];
+// h =True;
+document.write("<br>");
+document.write(typeof h);
